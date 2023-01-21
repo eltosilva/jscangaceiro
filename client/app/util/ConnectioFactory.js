e32e485b37965c73a684d8d0fa3b731cbac47a16
Elto Oliveira da Silva <petran.elto@gmail.com>
2023-01-21 15:08:52 -0300

    Final do capitulo 15

---
+++ client/app/util/ConnectioFactory.js
@@ -0,0 +1,59 @@
+const ConnectionFactory = (function(){
+
+  const stores = ['negociacoes']
+  /**@type {IDBDatabase} */
+  let connection = null
+  /**@type {Function} */
+  let close = null
+
+  return class ConnectionFactory {
+    constructor() {
+      throw new Error('Não é possível criar instâncias dessa classe')
+    }
+  
+    static getConnection() {
+      return new Promise((resolve, reject) => {
+        if(connection)
+          resolve(connection)
+        
+        /**@type {IDBOpenDBRequest} */
+        const openRequest = indexedDB.open('jscangaceiro', 2)
+  
+        /** @param {IDBVersionChangeEvent} e */
+        openRequest.onupgradeneeded = (e) => {
+          ConnectionFactory._createStores(e.target.result)
+        }
+        
+        /** @param {IDBVersionChangeEvent} e */
+        openRequest.onsuccess = (e) => {
+          if(!connection){
+            connection = e.target.result
+            close = connection.close.bind(connection)
+            connection.close = () => { throw new Error('Você não pode fechar diretamente a conexão')}
+          }
+
+          resolve(connection)
+        }
+        /** @param {IDBVersionChangeEvent} e */
+        openRequest.onerror = (e) => {
+          console.log(e.target.error)
+          reject(e.target.error.name)
+        }
+      })
+    }
+
+    /** @param {IDBDatabase} db */
+    static _createStores(db) {
+      stores.forEach(store => {
+        if (db.objectStoreNames.contains(store))
+          db.deleteObjectStore(store)
+        db.createObjectStore(store, { autoIncrement: true })
+      })
+    }
+
+    static closeConnection(){
+      if(connection)
+        close()
+    }
+  }
+})()
